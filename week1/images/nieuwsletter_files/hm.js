ac9755884cd2e943573a3b311c1070547aed9ce1
RobinHoeffnagel <57796323+RobinHoeffnagel@users.noreply.github.com>
2020-01-21 14:17:09 +0100

    Add files via upload

---
+++ week1/images/nieuwsletter_files/hm.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', animateHamburger);
+
+function animateHamburger() {
+	hamburger.classList.toggle('active');
+	navigation.classList.toggle('active');
+};
